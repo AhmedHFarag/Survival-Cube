3584cd74b24ad08cf686896e721a5278c807ddb8
Ahmed Farag <ahmed.farag91.af@gmail.com>
2016-11-03 16:10:13 +0800

    Controls fixed

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -11,7 +11,10 @@ public class InputManager : MonoBehaviour {
     public string horizontalAxisName = "Horizontal"; // The name given to the horizontal axis for the cross platform input
 
     private float m_xAxis;
-
+    private float m_lastDirection=0;
+    private bool updateHorizontal = true;
+    bool RightButton = false;
+    bool LeftButton = false;
     private static void OnMovementChanged(float xmovement)// Invoking the movement event
     {
         var handler = movementChanged;
@@ -31,9 +34,28 @@ public class InputManager : MonoBehaviour {
         CrossPlatformInputManager.RegisterVirtualAxis(HorizontalAxis);
         
     }
-    public void UpdateHorizontal(float v)
+    public void UpdateHorizontalRight(float v)
     {
-        HorizontalAxis.Update(v);
+        if(v>0.1f)
+        {
+            RightButton = true;
+        }
+        else
+        {
+            RightButton = false;
+        }
+        
+    }
+    public void UpdateHorizontalLeft(float v)
+    {
+        if (v > 0.1f)
+        {
+            LeftButton = true;
+        }
+        else
+        {
+            LeftButton = false;
+        }
 
     }
     void Update()
@@ -41,12 +63,24 @@ public class InputManager : MonoBehaviour {
 #if UNITY_EDITOR  //platform defines Run In Unity Only
 
         m_xAxis = Input.GetAxis("Horizontal");
-        #else
-        m_xAxis = CrossPlatformInputManager.GetAxis("Horizontal");
-        
+#else
+if(RightButton && !LeftButton)
+        {
+            HorizontalAxis.Update(1);
 
+        }
+else if(LeftButton && !RightButton)
+        {
+            HorizontalAxis.Update(-1);
+        }
+        
+        else if(!LeftButton && !RightButton)
+        {
+            HorizontalAxis.Update(0);
+        }
+        m_xAxis = CrossPlatformInputManager.GetAxis("Horizontal");
 #endif
- if (Mathf.Abs(m_xAxis)>0)
+        if (Mathf.Abs(m_xAxis)>0)
         {
             OnMovementChanged(m_xAxis);
         }
