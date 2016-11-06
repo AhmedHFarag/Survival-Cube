c1e93856c0138a15669c4b8413c0b90d3f4d7502
Ahmed Farag <ahmed.farag91.af@gmail.com>
2016-11-06 22:15:19 +0800

    Clearing pulse

---
+++ Assets/WaveClear.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveClear : MonoBehaviour {
+    public int Damage = 1000;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnParticleCollision(GameObject other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            other.GetComponent<Enemy>().TakeDamage(Damage);
+        }
+    }
+}
