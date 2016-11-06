5aafb72ccbc8e6c68f035e22384da5820d0dec37
yathrebshaher <yathrebshaher@gmail.com>
2016-11-06 18:19:30 +0200

    cooldown prefabs + script

---
+++ Assets/Scripts/Weapon/CoolDown.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class CoolDown : MonoBehaviour
+{
+    public Slider coolDownSlider;
+
+    float coolDownCounter = 0;
+    float coolDownMaxValue;
+
+    // Use this for initialization
+    void Start()
+    {
+        coolDownMaxValue = GameManager.Instance.weaponCoolDown;
+        coolDownSlider.maxValue = GameManager.Instance.weaponCoolDown;
+        coolDownSlider.value = coolDownSlider.maxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void WeaponCoolDownMethod()
+    {
+        Debug.Log("Hi");
+        GetComponent<EventTrigger>().enabled = false;
+        coolDownSlider.value = 0;
+        StartCoroutine(WeaponCoolDown());
+    }
+
+
+    IEnumerator WeaponCoolDown()
+    {
+
+        while (coolDownMaxValue > coolDownSlider.value)
+        {
+            // set slider 
+            coolDownSlider.value++;
+            yield return new WaitForSeconds(.5f);
+        }
+        Debug.Log("finished");
+        GetComponent<EventTrigger>().enabled= true;
+        StopCoroutine("WeaponCoolDown");
+
+    }
+}
