06873196a1a2cec3e40952ba0c5c845f90c31b26
DESKTOP-QFQE151\progm <prog.mohamedesmat@hotmail.com>
2016-11-07 22:23:32 +0800

    RotateEnimies

---
+++ Assets/Esmat/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+    private Transform ts;
+    private float rotationSpeed;
+    // Use this for initialization
+    void Start () {
+	ts  = GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+     
+        ts.Rotate(new Vector3(0, 1, 0), 2f);
+
+    
+
+    }
+}
