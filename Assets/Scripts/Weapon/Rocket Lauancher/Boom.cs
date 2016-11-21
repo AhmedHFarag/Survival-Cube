ce34ac583c880a1dd97cfb68a68759e9954a6750
Haitham <haithamhany7@gmail.com>
2016-11-21 20:03:37 +0200

    rocket launcher

---
+++ Assets/Scripts/Weapon/Rocket Lauancher/Boom.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boom : MonoBehaviour {
+
+    public int Damage;
+    void Update()
+    {
+
+        Destroy(gameObject, 2);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            other.GetComponent<Enemy>().TakeDamage(Damage);
+            
+        }
+        
+    }
+}
