cc51390ff668a0f4f23b687afd307e773ecc575d
Islam_Talha <islam_talha@hotmail.com>
2016-12-04 16:11:02 +0200

    Multiplay turrets

--- Assets/Scripts/TempWeapons/multiplyTurret.cs
+++ Assets/Scripts/TempWeapons/multiplyTurret.cs
@@ -5,48 +5,22 @@ using System.Collections.Generic;
 public class multiplyTurret : TempWeapon
 {
 
-    DefaultWeapon BasicWeapon;
-    public GameObject turret;
-    int turretCount = 3;
-    float Ellapsedtime = 0;
-    int i = 0;
-    private GameObject player;
-    float[] Angles = new float[3];
-    GameObject[] weapons = new GameObject[3];
     void Start()
     {
-        BasicWeapon = turret.GetComponent<DefaultWeapon>();
-        player = GameObject.FindGameObjectWithTag("Player");
-        Angles[0] = Player_Controller.Instance.WeaponPos.eulerAngles.y+90;
-        Angles[1] = Player_Controller.Instance.WeaponPos.eulerAngles.y - 90;
-        Angles[2] = Player_Controller.Instance.WeaponPos.eulerAngles.y + 180;
-    }
-    void OnEnable()
-    {
-        StartCoroutine(RemoveTurrets());
-    }
-    void FixedUpdate()
-    {
-        Ellapsedtime += Time.deltaTime;
+        GameObject _obj = Instantiate(GameManager.Instance.GetMainWeapon());
+        _obj.transform.position = transform.position;
+        _obj.transform.parent = this.transform;
+        _obj.transform.LookAt(transform.position - transform.forward);
 
-        if (i < turretCount && Ellapsedtime >= 1)
-        {
-            Ellapsedtime = 0;
-            GameObject _obj = Instantiate(turret);
-            _obj.transform.position = Player_Controller.Instance.WeaponPos.position ;
-            _obj.transform.parent = player.transform;
-            _obj.transform.Rotate(new Vector3(0, 1, 0), Angles[i]);
-            weapons[i] = _obj;
-            i++;
-        }
+        _obj = Instantiate(GameManager.Instance.GetMainWeapon());
+        _obj.transform.position = transform.position;
+        _obj.transform.parent = this.transform;
+        _obj.transform.LookAt(transform.position - transform.right);
+
+        _obj = Instantiate(GameManager.Instance.GetMainWeapon());
+        _obj.transform.position = transform.position;
+        _obj.transform.parent = this.transform;
+        _obj.transform.LookAt(transform.position+transform.right);
     }
-    IEnumerator RemoveTurrets()
-    {
-        yield return new WaitForSeconds(LifeTime);
-        foreach (GameObject obj in weapons)
-        {
-            Destroy(obj);
-        }
-        Debug.Log("Slow time destroued");
-    }
+
 }
