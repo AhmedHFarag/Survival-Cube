a1dc585310427e75f75fbd3ba5af438fecc346ad
DESKTOP-QFQE151\progm <prog.mohamedesmat@hotmail.com>
2017-01-09 14:24:38 +0200

    Terminated Script

---
+++ Assets/Scripts/Terminated.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terminated : MonoBehaviour {
+     AudioSource ter;
+	// Use this for initialization
+	void Awake ()
+    {
+        ter = GetComponent<AudioSource>();
+    }
+	
+	void OnEnable()
+    {
+        if (Player_Controller.Instance.HitPoints <= 0)
+        {
+                ter.Play();
+        }      
+        else
+        {
+            ter.Stop();
+        }
+    }
+}
