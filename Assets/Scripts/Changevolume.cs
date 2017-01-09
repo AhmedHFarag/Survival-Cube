7e0f4cbfcbfd1d03cd08786a748f2db373f21dcf
DESKTOP-QFQE151\progm <prog.mohamedesmat@hotmail.com>
2017-01-09 17:42:32 +0200

    Change master volume

--- Assets/Scripts/Changevolume.cs
+++ Assets/Scripts/Changevolume.cs
@@ -4,24 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Changevolume : MonoBehaviour {
-    private AudioSource bgs;
+   
     public Slider s;
     // Use this for initialization
     void Start () {
-   
-      
-        bgs = gameObject.GetComponent<AudioSource>();
         s.value = DataHandler.Instance.GetBgVolume();
-        bgs.volume = DataHandler.Instance.GetBgVolume();
+        AudioListener.volume= DataHandler.Instance.GetBgVolume();
     }
     public void ChangeVolume()
     {
-        
-        bgs.volume = s.value;
-        DataHandler.Instance.SetBgVolume(bgs.volume);
+        AudioListener.volume = s.value;
+        DataHandler.Instance.SetBgVolume(AudioListener.volume);
     }
-    // Update is called once per frame
-    void Update () {
-		
-	}
+  
 }
