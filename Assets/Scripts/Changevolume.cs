b691acbac0c675cb34f5c32fa7ab0c52b3bcfd6a
DESKTOP-QFQE151\progm <prog.mohamedesmat@hotmail.com>
2017-01-10 14:06:28 +0200

    Mute button

--- Assets/Scripts/Changevolume.cs
+++ Assets/Scripts/Changevolume.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Changevolume : MonoBehaviour {
-   
+
     public Slider s;
     // Use this for initialization
     void Start () {
         s.value = DataHandler.Instance.GetBgVolume();
         AudioListener.volume= DataHandler.Instance.GetBgVolume();
+  
     }
     public void ChangeVolume()
     {
         AudioListener.volume = s.value;
         DataHandler.Instance.SetBgVolume(AudioListener.volume);
     }
-  
+ public void Mute()
+    {
+        if (AudioListener.volume != 0)
+        {
+            AudioListener.volume = 0;
+            s.value = 0;
+            DataHandler.Instance.SetBgVolume(AudioListener.volume);
+        }
+           
+        else
+        {
+            s.value = 1;
+            AudioListener.volume = 1;
+            DataHandler.Instance.SetBgVolume(AudioListener.volume);
+        }
+            
+
+    }
 }
+    
