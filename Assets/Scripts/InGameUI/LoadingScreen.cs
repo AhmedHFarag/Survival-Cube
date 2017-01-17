e385a5e2d3d4513136c3697092344bf75919a1f9
Ahmed Farag <ahmed.farag91.af@gmail.com>
2017-01-17 19:00:18 +0200

    Loading screen

--- Assets/Scripts/InGameUI/LoadingScreen.cs
+++ Assets/Scripts/InGameUI/LoadingScreen.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class LoadingScreen : MonoBehaviour {
-    public List<string> Hints;
+    public string[] Hints;
+    public Text hint;
+    public Animator TextAnim;
 	// Use this for initialization
 	void Start () {
-		
+        if(TextAnim==null)
+        {
+            TextAnim = hint.gameObject.GetComponent<Animator>();
+        }
+        StartCoroutine("HintRoll");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    IEnumerator HintRoll()
+    {
+        int index = Random.Range(0, Hints.Length);
+        hint.text = Hints[index];
+        while(true)
+        {
+            yield return new WaitForSeconds(5);
+
+
+            hint.text = Hints[Random.Range(0, Hints.Length)];
+
+        }
+        yield return null;
+    }
 }
